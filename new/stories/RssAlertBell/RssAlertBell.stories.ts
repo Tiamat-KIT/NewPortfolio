1c4042612a8fa83518ac74f61676525e20e370df
Amo_Taiki_from_Desktop <c1101081@planet.kanazawa-it.ac.jp>
2024-02-05 18:54:37 +0900

    Update RssAlertBellStory args in RssAlertBell.stories.ts

--- new/stories/RssAlertBell/RssAlertBell.stories.ts
+++ new/stories/RssAlertBell/RssAlertBell.stories.ts
@@ -14,5 +14,7 @@ export default RssAlertBellMeta
 type RssAlertBellStory = StoryObj<typeof RssAlertBellMeta>
 
 export const ExampleRssAlertBellStory: RssAlertBellStory = {
-    args: {},
+    args: {
+        ZennRSS: []
+    },
 } 
